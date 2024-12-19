e8f0f7ff1d695118ae4a9e46d2078ce86344be60
Jonathan Strickland <djanatyn@gmail.com>
2024-12-19 14:52:28 -0500

    start fetching

--- fetch/src/main.rs
+++ fetch/src/main.rs
@@ -8,36 +8,75 @@
 
 use std::env;
 
+pub const SESSION_COOKIE_ENV_VAR: &str = "AOC_SESSION_COOKIE";
+
 /// Required application runtime configuration.
 #[derive(Debug)]
 pub struct Config {
-    /// The path of the current problem, e.g. "2024/day/1"
-    pub problem: String,
+    /// The path of the current problem, e.g. "2024/day/1".
+    pub path: String,
     /// A session cookie used to authenticate with Advent of Code.
     pub session_cookie: String,
 }
 
 impl Config {
+    const BASE_URL: &str = "https://adventofcode.com/";
+
     pub fn parse() -> Result<Self, String> {
         // parse cli args
-        let problem = match &env::args().collect::<Vec<String>>()[..] {
+        let path = match &env::args().collect::<Vec<String>>()[..] {
             [_, problem] => problem.clone(),
             [cmd, ..] => Err(format!("usage: {cmd} <YYYY/day/N>"))?,
             _ => Err("usage: fetch <YYYY/day/N>")?,
         };
         // parse SESSION_COOKIE
-        let session_cookie = match env::var("SESSION_COOKIE") {
+        let session_cookie = match env::var(SESSION_COOKIE_ENV_VAR) {
             Ok(secret) => secret,
-            Err(e) => Err(format!("please set SESSION_COOKIE: {e}"))?,
+            Err(e) => Err(format!("please set {SESSION_COOKIE_ENV_VAR}: {e}"))?,
         };
         Ok(Config {
-            problem,
+            path,
             session_cookie,
         })
+    }
+
+    fn description_url(&self) -> String {
+        String::from(Self::BASE_URL) + &self.path
+    }
+
+    fn input_url(&self) -> String {
+        self.description_url() + "/input"
+    }
+}
+
+/// Problem input and problem description (as HTML)
+#[derive(Debug)]
+pub struct Problem {
+    /// Description (HTML). Possibly includes part 2.
+    description: String,
+    /// Problem input (text).
+    input: String,
+}
+
+impl Problem {
+    fn request(config: &Config, path: String) -> Result<String, ureq::Error> {
+        Ok(ureq::get(&path)
+            .set(
+                "Cookie",
+                format!("session={}", config.session_cookie).as_str(),
+            )
+            .call()?
+            .into_string()?)
+    }
+
+    pub fn download(config: &Config) -> Result<Self, ureq::Error> {
+        let description = Self::request(config, config.description_url())?;
+        let input = Self::request(config, config.input_url())?;
+        Ok(Problem { description, input })
     }
 }
 
 fn main() -> Result<(), String> {
-    let _config = Config::parse()?;
+    // let _problem = Problem::download(&Config::parse()?);
     Ok(())
 }
