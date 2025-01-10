bbeda4e551a814431854ad45947b97bc22c2c94e
Jonathan Strickland <djanatyn@gmail.com>
2025-01-09 23:20:48 -0500

    day 1 part 2

--- solutions/src/bin/day01.rs
+++ solutions/src/bin/day01.rs
@@ -1,5 +1,13 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("day01.input");
+
+    println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
+}
+
+fn parse(input: &str) -> (Vec<i64>, Vec<i64>) {
     let pairs = input.lines().map(
         |line| match line.split_whitespace().collect::<Vec<&str>>()[..] {
             [first, second] => (
@@ -9,7 +17,11 @@ fn main() {
             _ => panic!("failed"),
         },
     );
-    let (mut left, mut right): (Vec<i64>, Vec<i64>) = pairs.unzip();
+    pairs.unzip()
+}
+
+fn part1(input: &str) -> i64 {
+    let (mut left, mut right) = parse(input);
     left.sort();
     right.sort();
 
@@ -19,5 +31,25 @@ fn main() {
         .map(|(left, right)| (left - right).abs())
         .sum();
 
-    println!("part 1: {}", total_distance);
+    total_distance
+}
+
+fn part2(input: &str) -> i64 {
+    let (left, right) = parse(input);
+    let mut counts: HashMap<i64, i64> = HashMap::new();
+    right.iter().for_each(|num| {
+        counts
+            .entry(*num)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    });
+    let total_similarity: i64 = left
+        .iter()
+        .map(|search| {
+            let count = counts.get(search).unwrap_or(&0);
+            count * search
+        })
+        .sum();
+
+    total_similarity
 }
