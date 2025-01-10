19e03cef1d91b173ab6a161d533adec456a1d730
Jonathan Strickland <djanatyn@gmail.com>
2025-01-09 22:34:19 -0500

    day 1 part 1

--- solutions/src/bin/day01.rs
+++ solutions/src/bin/day01.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("day01.input");
+    let pairs = input.lines().map(
+        |line| match line.split_whitespace().collect::<Vec<&str>>()[..] {
+            [first, second] => (
+                first.parse::<i64>().unwrap(),
+                second.parse::<i64>().unwrap(),
+            ),
+            _ => panic!("failed"),
+        },
+    );
+    let (mut left, mut right): (Vec<i64>, Vec<i64>) = pairs.unzip();
+    left.sort();
+    right.sort();
+
+    let total_distance: i64 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(left, right)| (left - right).abs())
+        .sum();
+
+    println!("part 1: {}", total_distance);
 }
